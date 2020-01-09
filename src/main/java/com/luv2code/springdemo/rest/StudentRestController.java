868c8967b2f67fa2c9c68b7d94d9d61a69f09b4b
Dmitrij <seo.wolf.d@gmail.com>
2020-01-09 20:06:04 +0100

    Spring REST - Exception Handling

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,9 +42,12 @@ public class StudentRestController {
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
 		
+		if ( (studentId >= listStudents.size())  || (studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found!!! - " + studentId);
+		}				
 		return listStudents.get(studentId);
+
 	}
-	
 	
 	
 	
